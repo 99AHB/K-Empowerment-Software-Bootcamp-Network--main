1054d07c64e15e1e96b96351e49af4d9431f333c
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-14 11:16:04 +0900

    SimpleEchoServer v0.3

--- SimpleEchoServer.java
+++ SimpleEchoServer.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -21,19 +22,19 @@ public class SimpleEchoServer {
                     PrintWriter pw =
                             new PrintWriter(clientSocket.getOutputStream(), true))
             {
-                Supplier<String> socketIn = () -> {
-                    try {
-                        return br.readLine();
-                    } catch (IOException ex) {
-                        return null;
-                    }
-                };
-                Stream s = Stream.generate(socketIn);
-                s.map(text -> {
-                    System.out.println("클라이언트로 부터 받은 메세지 : " + text);
-                    pw.println(text);
-                    return text;
-                }).allMatch(t -> t != null);
+                Stream
+                        .generate(() -> {
+                            try {
+                                return br.readLine();
+                            } catch (IOException ex) {
+                                return null;
+                            }
+                        })
+                        .peek(text -> {
+                            System.out.println("클라이언트로 부터 받은 메세지 : " + text);
+                            pw.println(text);
+                        })
+                        .allMatch(Objects::nonNull);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
