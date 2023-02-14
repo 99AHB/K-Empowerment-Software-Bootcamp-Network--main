81bd8f60d0d761eee01dded9b6150c82ce6f62e8
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-14 16:35:55 +0900

    ex01(수정전)

---
+++ ThreadEx.java
@@ -0,0 +1,26 @@
+class Worker extends Thread{
+    public void run(){
+        for(int i = 0; i<5; i++){
+            System.out.println("작업 스레드 : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+public class ThreadEx {
+    public static void main(String[] args) {
+        int alphabet = 'a';
+        Worker worker = new Worker();
+        for(int i = 0; i<26; i++){
+            System.out.println("메인 스레드 : " +(char)alphabet++);
+            try{
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
