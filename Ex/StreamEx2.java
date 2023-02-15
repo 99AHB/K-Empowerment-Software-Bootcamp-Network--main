6194a63b98b38e4be8f9517f61c345c56bbe6d42
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-15 17:34:10 +0900

    StreamEx02

--- Ex/StreamEx2.java
+++ Ex/StreamEx2.java
@@ -1,4 +1,26 @@
 package Ex;
 
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class StreamEx2 {
+    public static void main(String[] args) {
+        Stream<String> ss;
+        List<String> names = List.of("홍길동", "배장화", "임꺽정", "연흥부", "김선달", "황진이");
+        List<Integer> ages = List.of(25, 20, 29, 28, 32, 18);
+        List<Gender> genders = List.of(Gender.남, Gender.여, Gender.남, Gender.남, Gender.남, Gender.여);
+
+        Stream<Integer> si = ages.stream();
+        System.out.println(si.reduce(0, (a, b) -> a + b));
+        si = ages.stream();
+        System.out.println(si.max(Integer::compareTo).get());
+        IntStream is = ages.stream().mapToInt(a -> a.intValue());
+        System.out.println(is.average().getAsDouble());
+    }
 }
+
+enum Gender {
+    남, 여
+}
+
