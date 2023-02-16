b1110a12b40860894888358db6c44e706fcb2b7d
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-16 17:48:37 +0900

    연습문제 완료본

--- ExDataClient.java
+++ ExDataClient.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ExDataClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
         System.out.print("클라이언트 > 날짜 서버의 IP 주소는? ");
         String serverAddress = in.nextLine();
-        try(Socket client = new Socket(serverAddress, 9000));
+        try(Socket client = new Socket(serverAddress, 9000)){
             BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
         String answer = input.readLine();
         System.out.println(answer);
