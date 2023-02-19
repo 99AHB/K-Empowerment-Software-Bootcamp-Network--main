fb0305b7be1029a9500a67febe3b00716ff8833c
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-19 11:52:27 +0900

    ex19

---
+++ Ex19Product.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok. NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ex19Product {
+    private int no;
+    private String name;
+    private int price;
+    private int stock;
+}
